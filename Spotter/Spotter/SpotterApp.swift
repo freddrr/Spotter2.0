ed52e7454d749dae69eec8d2aa056a8a28a9eb84
freddrr <ftorres0448@gmail.com>
2023-06-12 15:59:09 -0500

    -First commit

---
+++ Spotter/Spotter/SpotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotterApp.swift
+//  Spotter
+//
+//  Created by Freddy Trae Torres on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
