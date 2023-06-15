b5e54773a0eb55647ade165a99351da9f935c3cb
emata1278 <130263445+emata1278@users.noreply.github.com>
2023-06-15 14:34:12 -0500

    Sign up view change

--- Spotter/Spotter/SignUpView.swift
+++ Spotter/Spotter/SignUpView.swift
@@ -8,52 +8,61 @@
 import Foundation
 import SwiftUI
 
+
 struct SignUpView : View {
     @ObservedObject var viewModel = SignUpViewModel()
     
     var body : some View {
         ZStack {
-            VStack {
+            Rectangle()
+                .frame(width:300, height: 500)
+                .cornerRadius(20)
+                .foregroundColor(Color("Custom Color"))
+                .overlay(
+            VStack(spacing: 30) {
                 Text("Welcome to Spotter")
                     .foregroundColor(.blue)
+                    .font(.title)
+                    .bold()
                     
                 TextField("Please enter a email", text: $viewModel.email)
                     .multilineTextAlignment(.center)
-                    .padding(7)
-                    .background(Color.white)
-                    .cornerRadius(8)
-                    .overlay(
+                    .background(
                           RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.gray, lineWidth: 1)
                                 .padding(.horizontal, 20)
+                                .frame(height: 35)
+                                .foregroundColor(Color.white)
                                     )
                 TextField("Please enter a username", text: $viewModel.username)
                     .multilineTextAlignment(.center)
-                    .padding(7)
-                    .background(Color.white)
-                    .cornerRadius(8)
-                    .overlay(
-                          RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.gray, lineWidth: 1)
-                                .padding(.horizontal, 20)
-                                    )
+                .background(
+                      RoundedRectangle(cornerRadius: 8)
+                            .padding(.horizontal, 20)
+                            .frame(height: 35)
+                            .foregroundColor(Color.white)
+                                )
                 SecureField("Please enter a password", text: $viewModel.password)
                     .multilineTextAlignment(.center)
-                    .padding(7)
-                    .background(Color.white)
-                    .cornerRadius(8)
-                    .overlay(
-                          RoundedRectangle(cornerRadius: 8)
-                                .stroke(Color.gray, lineWidth: 1)
-                                .padding(.horizontal, 20)
-                                    )
-                Button("Register") {
-                    viewModel.signIn()
+                .background(
+                      RoundedRectangle(cornerRadius: 8)
+//                            .stroke(Color.gray, lineWidth: 1)
+                            .padding(.horizontal, 20)
+                            .frame(height: 35)
+                            .foregroundColor(Color.white)
+                                )
+                ZStack{
+                    RoundedRectangle(cornerRadius: 8)
+                        .frame(width: 70, height: 35)
+                          .foregroundColor(Color.black)
+                              
+                    Button("Register") {
+                        viewModel.signIn()
+                    }
                 }
                    // NavigationLink("Already have an account?", value: SignInView())
                 
             }
             .padding()
-        }
+        )}
     }
 }
